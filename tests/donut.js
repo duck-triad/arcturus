116fc6ac308c33ab60832af4e4d47de45c8d6017
duck-triad <84645897+duck-triad@users.noreply.github.com>
2022-03-09 21:39:13 -0600

    Update donut.js

--- tests/donut.js
+++ tests/donut.js
@@ -7,7 +7,6 @@ function setup () {
     createCanvas(800,800, WEBGL);
     noStroke();
     pixelDensity(1);
-    //setAttributes('antialias', true);
     cam = createEasyCam();
     document.oncontextmenu = () => false;
 }
